116ff690407000ec2008971424d19084f8bdfa89
Jeremy Barthe <jeremy.barthe@gmail.com>
2013-02-20 23:51:22 +0100

    added dependency injection tests

---
+++ Tests/DependencyInjection/LexikMonologDoctrineExtensionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Lexik\Bundle\LexikMonologDoctrineBundle\Tests\DependencyInjection;
+
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Lexik\Bundle\LexikMonologDoctrineBundle\DependencyInjection\LexikMonologDoctrineExtension;
+
+class LexikMonologDoctrineExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConfigLoad()
+    {
+        $extension = new LexikMonologDoctrineExtension();
+        $extension->load(array($this->getConfig()), $container = new ContainerBuilder());
+
+        // parameters
+        $this->assertEquals('test_layout.html.twig', $container->getParameter('lexik_monolog_doctrine.base_layout'));
+        $this->assertEquals('logs', $container->getParameter('lexik_monolog_doctrine.doctrine.table_name'));
+        $this->assertInternalType('array', $container->getParameter('lexik_monolog_doctrine.doctrine.connection.configuration'));
+
+        // services
+        $this->assertTrue($container->hasDefinition('lexik_monolog_doctrine.doctrine_dbal.connection'));
+        $this->assertTrue($container->hasDefinition('lexik_monolog_doctrine.handler.doctrine_dbal'));
+    }
+
+    protected function getConfig()
+    {
+        return array(
+            'base_layout' => 'test_layout.html.twig',
+            'doctrine'    => array(
+                'table_name' => 'logs',
+                'connection' => array(
+                    'driver' => 'pdo_sqlite',
+                    'dbname' => 'monolog',
+                    'memory' => true,
+                ),
+            ),
+        );
+    }
+}
---
+++ Tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+}
+
+$autoload = require $autoloadFile;
+
+$autoload->loadClass('Doctrine\DBAL\DriverManager');
